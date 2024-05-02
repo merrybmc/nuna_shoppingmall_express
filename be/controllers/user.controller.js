e11ff232f5fd5d64589bb0e0bbd11225ce88e882
Byeong Min Cho <merrybmc@gmail.com>
2024-05-03 08:29:33 +0900

    signup be func

---
+++ be/controllers/user.controller.js
@@ -0,0 +1,26 @@
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+
+const userController = {};
+
+userController.createUser = async (req, res) => {
+  try {
+    const { email, password, name, level } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      throw new Error('이미 존재하는 이메일입니다.');
+    }
+
+    const salt = await bcrypt.genSaltSync(10);
+    const hash = await bcrypt.hash(password, salt);
+    const newUser = new User({ email, password, name, level });
+
+    await newUser.save();
+    return res.status(200).json({ status: 'ok' });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', error: err.message });
+  }
+};
+
+module.exports = userController;
