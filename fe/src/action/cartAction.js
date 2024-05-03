fc03e786a8dca8b1e60f0015ea700ea8b5e46f80
Byeong Min Cho <merrybmc@gmail.com>
2024-05-03 17:55:57 +0900

    basket fe

--- fe/src/action/cartAction.js
+++ fe/src/action/cartAction.js
@@ -1,9 +1,22 @@
-import api from "../utils/api";
-import * as types from "../constants/cart.constants";
-import { commonUiActions } from "../action/commonUiAction";
+import api from '../utils/api';
+import * as types from '../constants/cart.constants';
+import { commonUiActions } from '../action/commonUiAction';
 const addToCart =
   ({ id, size }) =>
-  async (dispatch) => {};
+  async (dispatch) => {
+    try {
+      dispatch({ type: types.ADD_TO_CART_REQUEST });
+      const res = await api.post('/cart', { productId: id, size, qty: 1 });
+
+      if (res.status !== 200) throw new Error(res.error);
+
+      dispatch({ type: types.ADD_TO_CART_SUCCESS, payload: res.data });
+      dispatch(commonUiActions.showToastMessage('카트 상품 추가', 'success'));
+    } catch (err) {
+      dispatch({ type: types.ADD_TO_CART_FAIL, error: err.error });
+      dispatch(commonUiActions.showToastMessage('카트 상품 실패', 'error'));
+    }
+  };
 
 const getCartList = () => async (dispatch) => {};
 const deleteCartItem = (id) => async (dispatch) => {};
--- fe/src/reducer/cartReducer.js
+++ fe/src/reducer/cartReducer.js
@@ -1,14 +1,21 @@
-import * as types from "../constants/cart.constants";
-import {
-  LOGIN_SUCCESS,
-  GOOGLE_LOGIN_SUCCESS,
-  LOGOUT,
-} from "../constants/user.constants";
+import * as types from '../constants/cart.constants';
+import { LOGIN_SUCCESS, GOOGLE_LOGIN_SUCCESS, LOGOUT } from '../constants/user.constants';
 
-const initialState = {};
+const initialState = {
+  loading: false,
+};
 
 function cartReducer(state = initialState, action) {
   const { type, payload } = action;
-  return state;
+  switch (type) {
+    case types.ADD_TO_CART_REQUEST:
+      return { ...state, loading: true };
+    case types.ADD_TO_CART_SUCCESS:
+      return { ...state };
+    case types.ADD_TO_CART_FAIL:
+      return { ...state, loading: false, error: payload };
+    default:
+      return state;
+  }
 }
 export default cartReducer;
