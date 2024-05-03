50e975e5512c389a5f2a827db4bf1b613d009d42
Byeong Min Cho <merrybmc@gmail.com>
2024-05-03 17:00:05 +0900

    product read be

--- be/controllers/product.controller.js
+++ be/controllers/product.controller.js
@@ -14,4 +14,13 @@ productController.createProduct = async (req, res) => {
   }
 };
 
+productController.getProduct = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ status: 'ok', data: products });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', error: err.message });
+  }
+};
+
 module.exports = productController;
--- be/routes/product.api.js
+++ be/routes/product.api.js
@@ -3,7 +3,12 @@ const authController = require('../controllers/auth.controller');
 const productController = require('../controllers/product.controller');
 const router = express.Router();
 
-router.post('/',,authController.authenticate, authController.checkAdminPermission, productController.createProduct);
-
+router.post(
+  '/',
+  authController.authenticate,
+  authController.checkAdminPermission,
+  productController.createProduct
+);
+router.get('/', productController.getProduct);
 
 module.exports;
