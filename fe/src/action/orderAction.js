4b23392098bf8cb7b1317ca257fd8c623f2b6c74
Byeong Min Cho <merrybmc@gmail.com>
2024-05-03 18:47:59 +0900

    order create fe

--- fe/src/action/orderAction.js
+++ fe/src/action/orderAction.js
@@ -1,9 +1,22 @@
-import api from "../utils/api";
-import * as types from "../constants/order.constants";
-import { cartActions } from "./cartAction";
-import { commonUiActions } from "./commonUiAction";
+import api from '../utils/api';
+import * as types from '../constants/order.constants';
+import { cartActions } from './cartAction';
+import { commonUiActions } from './commonUiAction';
 
-const createOrder = (payload) => async (dispatch) => {};
+const createOrder = (payload) => async (dispatch) => {
+  try {
+    dispatch({ type: types.CREATE_ORDER_REQUEST });
+
+    const res = await api.post('/order', payload);
+
+    if (res.status !== 200) throw new Error(res.error);
+
+    dispatch({ tpye: types.CREATE_ORDER_SUCCESS, payload });
+  } catch (err) {
+    dispatch({ type: types.CREATE_ORDER_FAIL, payload: err.error });
+    dispatch(commonUiActions.showToastMessage(err.error));
+  }
+};
 
 const getOrder = () => async (dispatch) => {};
 const getOrderList = (query) => async (dispatch) => {};
